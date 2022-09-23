7791c6c3a8da99f60008e03f206db6a0075420a3
HOANGANH\Lenovo <hoanganh5111999@gmail.com>
2022-09-23 16:53:29 +0700

    khởi tạo variadic, truyển slice vào variadic func, change slice

--- variadicFun.go
+++ variadicFun.go
@@ -1,10 +1,27 @@
 package main
 
-//variadic functions
-func addItem(item int, list ...int) {
+import "fmt"
 
+//variadic functions
+//khoi tao 1 variadic func
+func addItem(item int, list ...int) {
+	//100,200,300,400 -> int[]{100,200,300,400}
+	//[]int{1, 2, 3, 4} -> {int[] {1,2,3,4}}
+	list = append(list, item)
+	fmt.Println(list)
 }
 
+//change slice
+func change(list ...int) {
+	list[0] = 999
+}
 func main() {
+	addItem(1, 100, 200, 300, 400)
 
+	//pass 1 slice vao 1 variadic func
+	var list = []int{1, 2, 3, 4}
+	addItem(100, list...)
+
+	change(list...)
+	fmt.Println(list)
 }
